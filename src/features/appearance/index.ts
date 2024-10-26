19633732559735846e9c2ce742432abf7afee400
Denys Madureira <denysmb@zoho.com>
2024-10-26 13:15:01 +0100

    feat: add new style to horizontal sidebar

--- src/features/appearance/index.ts
+++ src/features/appearance/index.ts
@@ -231,7 +231,7 @@ const generateServiceRibbonWidthStyle = (
   return horizontal
     ? `
     .sidebar {
-      height: ${width}px !important;
+      height: ${width + 8}px !important;
       overflow: hidden !important;
     }
     .sidebar div {
@@ -250,15 +250,12 @@ const generateServiceRibbonWidthStyle = (
     .tab-item .tab-item__label {
       font-size: ${fontSize}px !important;
     }
-    .tab-item.is-label-enabled {
-      padding-top: 6px !important;
-      padding-bottom: 2px !important;
-    }
     .sidebar__button {
       font-size: ${width / 3}px !important;
+      line-height: 0;
     }
     .app .app__content {
-      padding-top: ${width + sidebarSizeBias}px !important;
+      padding-top: ${width + sidebarSizeBias + 8}px !important;
     }
     .workspaces-drawer {
       margin-top: -${width}px !important;
